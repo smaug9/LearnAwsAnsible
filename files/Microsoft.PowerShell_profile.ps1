d724c1dee501aed5c881545d27881f01da35d1d7
Jeff <jeff.wardlaw@gmail.com>
2020-04-25 08:45:35 -0400

    profile edits

--- files/Microsoft.PowerShell_profile.ps1
+++ files/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,13 @@
 $ProgressPreference = 'silentlycontinue'
 
 $env:Path += ';c:\users\jeff\bin'
+
+#disable get-credential popup
+$key = "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds"
+Set-ItemProperty $key ConsolePrompting True
+
+if (get-alias ls){ remove-item alias:\ls}
+
+function ls ([switch]$flash){
+ Get-ChildItem
+}
